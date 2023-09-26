43d97c21efaa02efade1b39084d10013c5a01cb3
Ankit Tanwar <tankitofficial01@gmail.com>
2023-09-26 11:50:18 +0530

    c1

--- src/app/island/page.js
+++ src/app/island/page.js
@@ -6,7 +6,7 @@ export default function island() {
     <section className='col-6 '>
       <h1 className='mt-4'>The Island</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+        Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley
         of type and scrambled it to make a type specimen book.</p>
     </section>
   </>
